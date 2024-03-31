6883f64e4246e4afa9f84c2916b7f985b9d8a4f0
zisan-2000 <zisan19393871@gmail.com>
2024-03-31 14:46:25 +0600

    second commit

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import logo from "./logo.svg";
+import "./App.css";
+import { LandingPage } from "./pages/LandingPage";
+
+function App() {
+  return <LandingPage />;
+}
+
+export default App;
---
+++ src/pages/LandingPage/index.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { ContactUsComp } from "../../components/ContactUsComp";
+import { FooterComp } from "../../components/FooterComp";
+import { HeaderComp } from "../../components/HeaderComp";
+import { LowestFareComp } from "../../components/LowestFareComp";
+import { ProfessionalExpComp } from "../../components/ProfessionalExpComp";
+import { ServicesComp } from "../../components/ServicesComp";
+import { WorldTourComp } from "../../components/WorlTourComp";
+
+export const LandingPage = () => {
+  return (
+    <div className="w-screen h-screen">
+      <ContactUsComp />
+      <HeaderComp />
+      <ServicesComp />
+      <LowestFareComp /> 
+      <ProfessionalExpComp /> 
+      <WorldTourComp />
+      <FooterComp />
+    </div>
+  );
+};
